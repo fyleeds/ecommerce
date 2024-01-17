c284918083d9ee8a7186e228cda2c57e4f565cb0
erxide <erwan.sinck12@gmail.com>
2024-01-17 19:05:36 +0000

    update

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -60,7 +60,7 @@ class RegistrationController extends AbstractController
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
-            'userEmail' => $user
+            'userEmail' => null
         ]);
     }
 }
